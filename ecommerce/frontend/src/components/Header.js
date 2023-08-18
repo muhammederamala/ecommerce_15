ed37917bc321c297a135b8ef85a095ea392f89e8
muhammederamala <muhammederamala15@gmail.com>
2023-08-18 23:19:13 +0530

    modified:   ecommerce/frontend/package-lock.json 	modified:   ecommerce/frontend/package.json 	modified:   ecommerce/frontend/public/favicon.ico 	new file:   ecommerce/frontend/public/images/airpods.jpg 	new file:   ecommerce/frontend/public/images/alexa.jpg 	new file:   ecommerce/frontend/public/images/camera.jpg 	new file:   ecommerce/frontend/public/images/mouse.jpg 	new file:   ecommerce/frontend/public/images/phone.jpg 	new file:   ecommerce/frontend/public/images/playstation.jpg 	new file:   ecommerce/frontend/public/images/sample.jpg 	modified:   ecommerce/frontend/public/index.html 	deleted:    ecommerce/frontend/src/App.css 	modified:   ecommerce/frontend/src/App.js 	deleted:    ecommerce/frontend/src/App.test.js 	new file:   ecommerce/frontend/src/bootstrap.min.css 	new file:   ecommerce/frontend/src/components/Footer.js 	new file:   ecommerce/frontend/src/components/Header.js 	new file:   ecommerce/frontend/src/components/Product.js 	new file:   ecommerce/frontend/src/components/Rating.js 	modified:   ecommerce/frontend/src/index.css 	modified:   ecommerce/frontend/src/index.js 	deleted:    ecommerce/frontend/src/logo.svg 	new file:   ecommerce/frontend/src/products.js 	new file:   ecommerce/frontend/src/screens/HomeScreen.js 	new file:   ecommerce/frontend/src/screens/ProductScreen.js 	deleted:    ecommerce/frontend/src/setupTests.js

---
+++ ecommerce/frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Nav, Navbar, Row, Container } from 'react-bootstrap'
+
+
+
+
+function Header() {
+  return (
+    <header>
+        <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+            <Container>
+                    <Navbar.Brand>ProShop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link ><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                        <Nav.Link>Login</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>    
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
