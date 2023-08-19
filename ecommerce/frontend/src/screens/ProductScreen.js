787d2c0aa77f3a669b393275d28ad4fd4de97691
muhammederamala <muhammederamala15@gmail.com>
2023-08-20 00:09:40 +0530

    modified:   ecommerce/frontend/package-lock.json 	modified:   ecommerce/frontend/package.json 	modified:   ecommerce/frontend/src/screens/HomeScreen.js 	modified:   ecommerce/frontend/src/screens/ProductScreen.js

--- ecommerce/frontend/src/screens/ProductScreen.js
+++ ecommerce/frontend/src/screens/ProductScreen.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
 import { Link, useParams } from 'react-router-dom';
+import axios from 'axios'
 
 
 
-
+// components
 import Rating from '../components/Rating'
 import products from '../products'
 
@@ -12,7 +13,24 @@ import products from '../products'
 function ProductScreen() {
 
     const { id } = useParams();
-    const product = products.find((p) => String(p._id) === id);
+
+    const [product , setProduct] = useState([])
+    
+
+    useEffect(() => {
+
+        async function fetchProduct(){
+            const { data } = await axios.get(`/api/products/${id}`)
+            setProduct(data)
+
+        }
+
+        fetchProduct()
+
+    },[])
+
+   
+    // const product = products.find((p) => String(p._id) === id);
   
   
     return (
