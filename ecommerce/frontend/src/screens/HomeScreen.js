899313ec37e3b69911de72b42a2cea9353dd7b58
muhammederamala <muhammederamala15@gmail.com>
2023-08-21 13:25:36 +0530

    new file:   ecommerce/frontend/src/actions/productActions.js 	new file:   ecommerce/frontend/src/components/Loader.js 	new file:   ecommerce/frontend/src/components/Message.js 	new file:   ecommerce/frontend/src/constants/productConstants.js 	modified:   ecommerce/frontend/src/reducers/productReducers.js 	modified:   ecommerce/frontend/src/screens/HomeScreen.js

--- ecommerce/frontend/src/screens/HomeScreen.js
+++ ecommerce/frontend/src/screens/HomeScreen.js
@@ -1,42 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
-import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 
 import Product from '../components/Product'
-
+import { ListProducts } from '../actions/productActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 
 
 function HomeScreen() {
-
-    const [products , setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { error, loading, products } = productList
     
 
     useEffect(() => {
 
-        async function fetchProducts(){
-            const { data } = await axios.get('/api/products/')
-            setProducts(data)
+    dispatch(ListProducts())
 
-        }
-
-        fetchProducts()
-
-    },[])
-
+    }, [dispatch])
   return (
     <div>
             <h1>Latest Products</h1>
-                    <div>
-                        <Row>
-                            {products.map(product => (
-                                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                    <Product product={product} />
-                                </Col>
-                            ))}
-                        </Row>
-                    </div>
+                {loading ? <Loader />
+                    : error ? <Message variant='danger' >{error}</Message> 
+                    : <div>
+                    <Row>
+                        {products.map(product => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+                }
+                    
             
         </div>
   )
