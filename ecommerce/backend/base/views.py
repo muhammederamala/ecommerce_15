3911438436729b9afd8ae486392393222e2f71a8
muhammederamala <muhammederamala15@gmail.com>
2023-08-23 00:07:34 +0530

    modified:   ecommerce/backend/backend/__pycache__/settings.cpython-39.pyc 	modified:   ecommerce/backend/backend/settings.py 	modified:   ecommerce/backend/base/__pycache__/serializer.cpython-39.pyc 	modified:   ecommerce/backend/base/__pycache__/urls.cpython-39.pyc 	modified:   ecommerce/backend/base/__pycache__/views.cpython-39.pyc 	modified:   ecommerce/backend/base/serializer.py 	modified:   ecommerce/backend/base/urls.py 	modified:   ecommerce/backend/base/views.py 	modified:   ecommerce/backend/db.sqlite3

--- ecommerce/backend/base/views.py
+++ ecommerce/backend/base/views.py
@@ -5,11 +5,35 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
-from .serializer import ProductSerializer
+from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+	def validate(self, attrs):
+		data = super().validate(attrs)
+
+		serializer = UserSerializerWithToken(self.user).data
+
+		for k,v in serializer.items():
+			data[k] =v
+		
+		return data	
+
+class MyTokenObtainPairView(TokenObtainPairView):
+	serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
 	return Response('Hello')
+
+
+@api_view(['GET'])
+def getUserProfile(request):
+	user = request.user
+	serializer = UserSerializer(user, many=False)
+	return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -24,3 +48,4 @@ def getProduct(request,pk):
 	product = Product.objects.get(_id=pk)
 	serializer = ProductSerializer(product, many = False)
 	return Response(serializer.data)
+
