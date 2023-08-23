2e7b9791bd0b21cb0912cb70aa3b549c0fd85cae
muhammederamala <muhammederamala15@gmail.com>
2023-08-23 23:02:58 +0530

    modified:   ecommerce/backend/backend/__pycache__/urls.cpython-39.pyc 	modified:   ecommerce/backend/backend/urls.py 	modified:   ecommerce/backend/base/__pycache__/apps.cpython-39.pyc 	new file:   ecommerce/backend/base/__pycache__/signals.cpython-39.pyc 	modified:   ecommerce/backend/base/__pycache__/views.cpython-39.pyc 	modified:   ecommerce/backend/base/apps.py 	new file:   ecommerce/backend/base/signals.py 	deleted:    ecommerce/backend/base/urls.py 	new file:   ecommerce/backend/base/urls/__init__.py 	new file:   ecommerce/backend/base/urls/__pycache__/__init__.cpython-39.pyc 	new file:   ecommerce/backend/base/urls/__pycache__/order_urls.cpython-39.pyc 	new file:   ecommerce/backend/base/urls/__pycache__/product_urls.cpython-39.pyc 	new file:   ecommerce/backend/base/urls/__pycache__/user_urls.cpython-39.pyc 	new file:   ecommerce/backend/base/urls/order_urls.py 	new file:   ecommerce/backend/base/urls/product_urls.py 	new file:   ecommerce/backend/base/urls/user_urls.py 	new file:   ecommerce/backend/base/views/__init__.py 	new file:   ecommerce/backend/base/views/__pycache__/__init__.cpython-39.pyc 	new file:   ecommerce/backend/base/views/__pycache__/order_views.cpython-39.pyc 	new file:   ecommerce/backend/base/views/__pycache__/product_views.cpython-39.pyc 	new file:   ecommerce/backend/base/views/__pycache__/user_views.cpython-39.pyc 	new file:   ecommerce/backend/base/views/order_views.py 	new file:   ecommerce/backend/base/views/product_views.py 	renamed:    ecommerce/backend/base/views.py -> ecommerce/backend/base/views/user_views.py 	modified:   ecommerce/backend/db.sqlite3

--- ecommerce/backend/base/views/user_views.py
+++ ecommerce/backend/base/views/user_views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-
+from django.contrib.auth.hashers import make_password # We cannot store password in raw form.
+from rest_framework import status #custom error message.
 from django.contrib.auth.models import User
-from .models import Product
-from .products import products
-from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
+from base.serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
+
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -29,30 +28,27 @@ class MyTokenObtainPairView(TokenObtainPairView):
 	serializer_class = MyTokenObtainPairSerializer
 
 
-from django.contrib.auth.hashers import make_password # We cannot store password in raw form.
-@api_view(['GET'])
+
+@api_view(['POST'])
 def registerUser(request):
 	data = request.data
 
-	user = User.objects.create(
-		first_name = data['name'],
-		username = data['email'],
-		email = data['email'],
-		password = make_password(data['password'])
-	)
-	serializer = UserSerializerWithToken(user, many=False)
+	try:		
+		user = User.objects.create(
+			first_name = data['name'],
+			username = data['email'],
+			email = data['email'],
+			password = make_password(data['password'])
+		)
+		serializer = UserSerializerWithToken(user, many=False)
 
-	return Response(serializer.data)
-
-
-
-
-@api_view(['GET'])
-def getRoutes(request):
-	return Response('Hello')
+		return Response(serializer.data)
+	except:
+		message = {'detail': 'User with this email already exists'}
+		return Response(message, status = status.HTTP_400_BAD_REQUEST)
 
 
-
+# profile a the logged in user. The user profile is retrieved through JWT.
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def getUserProfile(request):
@@ -69,22 +65,3 @@ def getUsers(request):
 	users = User.objects.all()
 	serializer = UserSerializer(users, many=True)
 	return Response(serializer.data)
-
-
-
-
-@api_view(['GET'])
-def getProducts(request):
-	products = Product.objects.all()
-	serializer = ProductSerializer(products, many=True)
-	return Response(serializer.data)
-
-
-
-
-@api_view(['GET'])
-def getProduct(request,pk):
-	product = Product.objects.get(_id=pk)
-	serializer = ProductSerializer(product, many = False)
-	return Response(serializer.data)
-
