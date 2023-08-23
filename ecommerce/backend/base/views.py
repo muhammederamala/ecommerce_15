f8bd49fd1c03018d0d4b541ca121494f65e8b430
muhammederamala <muhammederamala15@gmail.com>
2023-08-23 18:45:34 +0530

    modified:   ecommerce/backend/base/__pycache__/urls.cpython-39.pyc 	modified:   ecommerce/backend/base/__pycache__/views.cpython-39.pyc 	modified:   ecommerce/backend/base/urls.py 	modified:   ecommerce/backend/base/views.py

--- ecommerce/backend/base/views.py
+++ ecommerce/backend/base/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
+from django.contrib.auth.models import User
 from .models import Product
 from .products import products
 from .serializer import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 	def validate(self, attrs):
@@ -24,16 +28,49 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
 	serializer_class = MyTokenObtainPairSerializer
 
+
+from django.contrib.auth.hashers import make_password # We cannot store password in raw form.
+@api_view(['GET'])
+def registerUser(request):
+	data = request.data
+
+	user = User.objects.create(
+		first_name = data['name'],
+		username = data['email'],
+		email = data['email'],
+		password = make_password(data['password'])
+	)
+	serializer = UserSerializerWithToken(user, many=False)
+
+	return Response(serializer.data)
+
+
+
+
 @api_view(['GET'])
 def getRoutes(request):
 	return Response('Hello')
 
 
+
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
 	user = request.user
 	serializer = UserSerializer(user, many=False)
 	return Response(serializer.data)
+
+
+
+# list of all users. This can be viewed by admin.
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+	users = User.objects.all()
+	serializer = UserSerializer(users, many=True)
+	return Response(serializer.data)
+
+
 
 
 @api_view(['GET'])
@@ -41,6 +78,8 @@ def getProducts(request):
 	products = Product.objects.all()
 	serializer = ProductSerializer(products, many=True)
 	return Response(serializer.data)
+
+
 
 
 @api_view(['GET'])
